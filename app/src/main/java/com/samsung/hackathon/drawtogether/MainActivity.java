541f9511263620742e385cc38841bc76845f2f8a
Rexa <zhaoweic18@gmail.com>
2016-09-06 22:45:39 +0900

    프로젝트 설정 및 Pen SDK 추가

---
+++ app/src/main/java/com/samsung/hackathon/drawtogether/MainActivity.java
@@ -0,0 +1,30 @@
+package com.samsung.hackathon.drawtogether;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        App.L.d("onCreate()");
+        ButterKnife.bind(this);
+    }
+
+    public void onMoveToDrawActivity(final View view)
+    {
+        App.L.d("onMoveToDrawActivity()");
+        final Intent i = new Intent(this, DrawActivity.class);
+        startActivity(i);
+
+    }
+}
