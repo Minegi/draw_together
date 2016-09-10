5d8d29549eabab83bda97337dfe42a56d4bc9c0b
Rexa <zhaoweic18@gmail.com>
2016-09-10 22:56:39 +0900

    지우개 선택 버튼 추가

--- app/src/main/java/com/samsung/hackathon/drawtogether/DrawActivity.java
+++ app/src/main/java/com/samsung/hackathon/drawtogether/DrawActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -14,11 +15,15 @@ import android.widget.Toast;
 
 import com.samsung.android.sdk.SsdkUnsupportedException;
 import com.samsung.android.sdk.pen.Spen;
+import com.samsung.android.sdk.pen.SpenSettingEraserInfo;
 import com.samsung.android.sdk.pen.SpenSettingPenInfo;
+import com.samsung.android.sdk.pen.SpenSettingViewInterface;
 import com.samsung.android.sdk.pen.document.SpenNoteDoc;
 import com.samsung.android.sdk.pen.document.SpenPageDoc;
 import com.samsung.android.sdk.pen.engine.SpenColorPickerListener;
 import com.samsung.android.sdk.pen.engine.SpenSurfaceView;
+import com.samsung.android.sdk.pen.engine.SpenTouchListener;
+import com.samsung.android.sdk.pen.settingui.SpenSettingEraserLayout;
 import com.samsung.android.sdk.pen.settingui.SpenSettingPenLayout;
 import com.samsung.hackathon.drawtogether.util.SPenSdkUtils;
 
@@ -34,13 +39,18 @@ public class DrawActivity extends AppCompatActivity {
 
     private boolean mIsSpenFeatureEnabled;
 
+    // constants
+    private final int SPEN = SpenSettingViewInterface.TOOL_SPEN;
+
     // layouts
     private SpenSettingPenLayout mPenSettingView;
+    private SpenSettingEraserLayout mEraserSettingView;
     private FrameLayout mSpenViewContainer;
     private RelativeLayout mSpenViewLayout;
 
     // buttons
     private ImageView mPenBtn;
+    private ImageView mEraserBtn;
 
     // listeners
     private final View.OnClickListener mPenBtnClickListener = new View.OnClickListener() {
@@ -50,8 +60,25 @@ public class DrawActivity extends AppCompatActivity {
                 mPenSettingView.setVisibility(View.GONE);
             } else {
                 mPenSettingView.setViewMode(SpenSettingPenLayout.VIEW_MODE_NORMAL);
+                // TODO: 나타날 때 포지션 지정 (setPosition)
                 mPenSettingView.setVisibility(View.VISIBLE);
             }
+            mSpenSurfaceView.setToolTypeAction(SPEN, SpenSurfaceView.ACTION_STROKE);
+            selectButton(mPenBtn);
+        }
+    };
+
+    private final View.OnClickListener mEraserBtnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (mEraserSettingView.isShown()) {
+                mEraserSettingView.setVisibility(View.GONE);
+            } else {
+                // TODO: 나타날 때 포지션 지정 (setPosition)
+                mEraserSettingView.setVisibility(View.VISIBLE);
+            }
+            mSpenSurfaceView.setToolTypeAction(SPEN, SpenSurfaceView.ACTION_ERASER);
+            selectButton(mEraserBtn);
         }
     };
 
@@ -63,6 +90,34 @@ public class DrawActivity extends AppCompatActivity {
                 penInfo.color = color;
                 mPenSettingView.setInfo(penInfo);
             }
+        }
+    };
+
+    private final SpenSettingEraserLayout.EventListener mEraserListener =
+            new SpenSettingEraserLayout.EventListener() {
+        @Override
+        public void onClearAll() {
+            mSpenPageDoc.removeAllObject();
+            mSpenSurfaceView.update();
+        }
+    };
+
+    private SpenTouchListener mPreTouchSurfaceViewListener = new SpenTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            // Drawing 중에는 버튼을 disable 시킨다
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    enableButton(false);
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                case MotionEvent.ACTION_UP:
+                    enableButton(true);
+                    break;
+            }
+
+            return false;
         }
     };
 
@@ -81,14 +136,9 @@ public class DrawActivity extends AppCompatActivity {
         createSpenSurfaceView();
         createSpenNoteDoc();
         createSpenPageDoc();
-        initializePenSettingInfo();
+        initializeSettingInfo();
 
         initializeOtherViews();
-    }
-
-    private void initializeOtherViews() {
-        mPenBtn = (ImageView) findViewById(R.id.pen_btn);
-        mPenBtn.setOnClickListener(mPenBtnClickListener);
     }
 
     @Override
@@ -112,6 +162,14 @@ public class DrawActivity extends AppCompatActivity {
                 mSpenNoteDoc = null;
             }
         }
+
+        if (mPenSettingView != null) {
+            mPenSettingView.close();
+        }
+
+        if (mEraserSettingView != null) {
+            mEraserSettingView.close();
+        }
     }
 
     private void initializeSpenSdk() {
@@ -125,11 +183,13 @@ public class DrawActivity extends AppCompatActivity {
             App.L.d("isSpenFeatureEnabled=" + mIsSpenFeatureEnabled);
         } catch (SsdkUnsupportedException e) {
             if (SPenSdkUtils.processUnsupportedException(this, e)) {
-                return;
+                Toast.makeText(mContext, "This device cannot support Spen SDK.",
+                        Toast.LENGTH_LONG).show();
+                finish();
             }
         } catch (Exception e) {
             Toast.makeText(mContext, "Cannot initialize Spen SDK.",
-                    Toast.LENGTH_SHORT).show();
+                    Toast.LENGTH_LONG).show();
             e.printStackTrace();
             finish();
         }
@@ -139,7 +199,10 @@ public class DrawActivity extends AppCompatActivity {
         App.L.d("createSpenSettingView()");
 
         mPenSettingView = new SpenSettingPenLayout(mContext, "", mSpenViewLayout);
+        mEraserSettingView = new SpenSettingEraserLayout(mContext, "", mSpenViewLayout);
+
         mSpenViewContainer.addView(mPenSettingView);
+        mSpenViewContainer.addView(mEraserSettingView);
     }
 
     private void createSpenSurfaceView() {
@@ -153,8 +216,11 @@ public class DrawActivity extends AppCompatActivity {
         }
 
         mSpenSurfaceView.setToolTipEnabled(true);
+        mSpenSurfaceView.setPreTouchListener(mPreTouchSurfaceViewListener);
         mSpenViewLayout.addView(mSpenSurfaceView);
+
         mPenSettingView.setCanvasView(mSpenSurfaceView);
+        mEraserSettingView.setCanvasView(mSpenSurfaceView);
     }
 
     private void createSpenNoteDoc() {
@@ -174,26 +240,81 @@ public class DrawActivity extends AppCompatActivity {
 
     private void createSpenPageDoc() {
         App.L.d("createSpenPageDoc()");
+        // append first page
         mSpenPageDoc = mSpenNoteDoc.appendPage();
         mSpenPageDoc.setBackgroundColor(0xffffff);
         mSpenPageDoc.clearHistory();
+
         mSpenSurfaceView.setPageDoc(mSpenPageDoc, true);
 
+        // 아직까지는 Spen 모드만 지원한다
         if (!mIsSpenFeatureEnabled) {
             mSpenSurfaceView.setToolTypeAction(SpenSurfaceView.TOOL_FINGER,
                     SpenSurfaceView.ACTION_STROKE);
-            Toast.makeText(mContext, "Device does not support Spen.\n" +
-                    "You can draw strokes with your finger.", Toast.LENGTH_LONG).show();
+            Toast.makeText(mContext, "Device does not support Spen.", Toast.LENGTH_LONG).show();
+            finish();
         }
     }
 
-    private void initializePenSettingInfo() {
-        App.L.d("initializePenSettingInfo()");
+    private void initializeSettingInfo() {
+        App.L.d("initializeSettingInfo()");
+
+        // pen setting
         final SpenSettingPenInfo penInfo = new SpenSettingPenInfo();
         penInfo.color = Color.BLACK;
         penInfo.size = 10;
         mSpenSurfaceView.setPenSettingInfo(penInfo);
         mPenSettingView.setInfo(penInfo);
+
+        // erase setting
+        final SpenSettingEraserInfo eraserInfo = new SpenSettingEraserInfo();
+        eraserInfo.size = 30;
+        mSpenSurfaceView.setEraserSettingInfo(eraserInfo);
+        mEraserSettingView.setInfo(eraserInfo);
+
+        // register listeners
         mSpenSurfaceView.setColorPickerListener(mColorPickerListener);
+        mEraserSettingView.setEraserListener(mEraserListener);
+
+        // set stroke action
+        mSpenSurfaceView.setToolTypeAction(SpenSettingViewInterface.TOOL_SPEN,
+                SpenSurfaceView.ACTION_STROKE);
+    }
+
+    private void initializeOtherViews() {
+        App.L.d("initializeOtherViews()");
+        mPenBtn = (ImageView) findViewById(R.id.pen_btn);
+        mPenBtn.setOnClickListener(mPenBtnClickListener);
+
+        mEraserBtn = (ImageView) findViewById(R.id.eraser_btn);
+        mEraserBtn.setOnClickListener(mEraserBtnClickListener);
+
+        selectButton(mPenBtn);
+    }
+
+    private void selectButton(final View view) {
+        mPenBtn.setSelected(false);
+        mEraserBtn.setSelected(false);
+
+        view.setSelected(true);
+
+        // 선택한 버튼 이외의 나머지 설정 뷰들은 다 닫는다
+        closeOtherSettingView(view);
+    }
+
+    private void enableButton(final boolean isEnable) {
+        mPenBtn.setEnabled(isEnable);
+        mEraserBtn.setEnabled(isEnable);
+    }
+
+    private void closeOtherSettingView(final View view) {
+
+        if (view.getId() != mPenBtn.getId()) {
+            mPenSettingView.setVisibility(SpenSurfaceView.GONE);
+        }
+
+        if (view.getId() != mEraserBtn.getId()) {
+            mEraserSettingView.setVisibility(SpenSurfaceView.GONE);
+        }
     }
 }
